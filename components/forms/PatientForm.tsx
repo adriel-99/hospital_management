eec161a617781cf4e7b9c1f4c75ea19b9de521c3
adriel-99 <adrielmadan@gmail.com>
2024-07-26 23:02:20 +0200

    setting up the user form validation and datbase from write app

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,58 +5,101 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import {Form,} from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+
+import { UserFormValidation } from "@/lib/validation"
+
+import { useRouter } from "next/navigation"
+
+export enum FormFieldType {
+    INPUT = "input",
+    TEXTAREA = "textarea",
+    PHONE_INPUT = "phoneInput",
+    CHECKBOX = "checkbox",
+    DATE_PICKER = "datePicker",
+    SELECT = "select",
+    SKELETON = "skeleton"
+}
+
  
 const  PatientForm = () => {
+
+    const router = useRouter()
+
+    const [isLoading, setIsLoading] = useState(false)
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+        name: "",
+        email: "",
+        phone: "",
+        },
+    })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true)
+
+    try {
+
+      // const userData = {name, email, phone}
+      // const user = await createUser(userData)
+
+      // if (user) {
+      //   router.push(`/patients/${user.$id}/register`)
+      // }
+
+    } catch (error) {
+        console.log(error)
+    }
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi There 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+
+        <CustomFormField 
           control={form.control}
+          fieldType={FormFieldType.INPUT}
           name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          label="Full Name"
+          placeholder="Enter your full name"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="User"
         />
-        <Button type="submit">Submit</Button>
+
+        <CustomFormField 
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          label="Email"
+          placeholder="Enter your email"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="Email"
+        />
+        
+        <CustomFormField 
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone Number"
+          placeholder="Enter your phone number"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="Email"
+        />
+
+        <SubmitButton isLoading={isLoading}>
+            Get Started
+        </SubmitButton>
       </form>
     </Form>
   )
